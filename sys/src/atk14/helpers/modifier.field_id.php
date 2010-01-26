c893ab3f487d5c4265cf29e782679f1a1748c145
yarri <jaromir.tomek@gmail.com>
2010-01-26 09:45:52 +0000

    new helper field_id

---
+++ sys/src/atk14/helpers/modifier.field_id.php
@@ -0,0 +1,10 @@
+<?
+/**
+ * Zjisti id policka.
+ * <label for="{$form|field_id:"usename"}">....</label>
+ */
+function smarty_modifier_field_id($form, $field)
+{
+    $field = $form->get_field($field);
+    return $field->id_for_label();
+}
