b7d1796222c11e2b9d6fd8c4689379fd5be1583e
yarri <jaromir.tomek@gmail.com>
2011-01-10 17:34:16 +0000

    a migration should be a php class now

--- sys/src/scripts/migrate.php
+++ sys/src/scripts/migrate.php
@@ -1,5 +1,19 @@
 #!/usr/bin/env php
 <?
+/**
+* Do pending migrations.
+* Load and process *.sql or *.inc files from the migration folder (db/migrations/).
+* 
+* Usage:
+* 	$ php migrate.php
+* 	$ ATK14_ENV=production php migrate.php
+*
+* Note: table schema_magrations needs to be created in the database.
+*		CREATE TABLE schema_magrations(
+*			version VARCHAR(255) PRIMARY KEY
+*		);
+*/
+
 require_once(dirname(__FILE__)."/load.inc");
 
 $logger = &Atk14Migration::GetLogger();
@@ -8,14 +22,14 @@ $logger = &Atk14Migration::GetLogger();
 $migrations = array();
 $dir = opendir($ATK14_GLOBAL->getMigrationsPath());
 while($item = readdir($dir)){
-	if(preg_match("/(.+)\\.sql$/",$item,$matches)){
+	if(preg_match("/(.+)\\.(sql|inc)$/",$item,$matches)){
 		$migrations[] = $item;
 	}
 }
 closedir($dir);
 asort($migrations);
 
-// getting list of migration done migrations
+// getting list of done migrations
 $already_done_migrations = $dbmole->selectIntoArray("SELECT version FROM schema_magrations ORDER BY version");
 
 $counter = 0;
@@ -23,8 +37,16 @@ foreach($migrations as $m){
 	if(in_array($m,$already_done_migrations)){ continue; }
 
 	$logger->info("about to start migration $m"); $logger->flush();
+	
+	if(preg_match("/^[0-9]+_(.*)\\.inc$/",$m,$matches)){
+		require_once($ATK14_GLOBAL->getMigrationsPath().$m);
+		$class_name = preg_replace("/_/","",$matches[1]);
+		$migr = new $class_name($m);
+	}else{
+		// an *.sql file
+		$migr = new Atk14MigrationBySqlFile($m);
+	}
 
-	$migr = new Atk14MigrationBySqlFile($m);
 	if(!$migr->migrateUp()){
 		break; // an error occured
 	}
