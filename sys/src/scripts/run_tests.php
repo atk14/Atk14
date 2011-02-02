2a91e44002f6b80b1c59ec3c1ae910a705185138
yarri <jaromir.tomek@gmail.com>
2011-02-02 13:49:13 +0000

    workaround around testing

--- sys/src/scripts/run_tests.php
+++ sys/src/scripts/run_tests.php
@@ -1,12 +1,24 @@
 #!/usr/bin/env php
 <?
+/**
+* Run application`s tests.
+*
+* Due to some backlog must be run this way:
+* 	$ php ./scripts/run_tests.php
+*
+* This will probably fail:
+* 	$ ./scripts/run_tests.php
+*/
+
 require_once(dirname(__FILE__)."/load.inc");
 
 $directory_with_tests = $ATK14_GLOBAL->getApplicationPath()."/../test";
 $script_directory = dirname(__FILE__);
 
-$command = "cd $directory_with_tests/models/; /usr/bin/env php $script_directory/run_unit_tests.php";
-echo `$command`;
+foreach(array("models","controllers") as $d){
+	$command = "cd $directory_with_tests/$d/; $script_directory/run_unit_tests.php";
+	echo "-----------\ngonna run:\n$command\n\n";
+	passthru($command);
+}
 
-$command = "cd $directory_with_tests/controllers/; /usr/bin/env php $script_directory/run_unit_tests.php";
-echo `$command`;
+
