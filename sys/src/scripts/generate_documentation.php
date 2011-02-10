88a3e8e9703f08626473f7c2635b3207214b0e38
mysutka <michal.vojan@gmail.com>
2011-02-10 17:32:11 +0000

    small improvement

--- sys/src/scripts/generate_documentation.php
+++ sys/src/scripts/generate_documentation.php
@@ -16,16 +16,28 @@
 
 require_once(dirname(__FILE__)."/load.inc");
 
-$src_dir = $ATK14_GLOBAL->getApplicationPath()."../sys/src/atk14";
+$src_dirs = array();
+$src_dirs[] = $ATK14_GLOBAL->getApplicationPath()."../sys/src/atk14";
+$src_dirs[] = $ATK14_GLOBAL->getApplicationPath()."../sys/src/http";
+
 $output_dir = $ATK14_GLOBAL->getApplicationPath()."../tmp/documentation";
 
 if (!file_exists($output_dir)) {
 	files::mkdir($output_dir, &$err, &$err_str);
 }
 
+$src_dir = join(",", $src_dirs);
 $command = "phpdoc -o HTML:frames:DOM/phpdoc.de -d $src_dir -t $output_dir";
 
-passthru($command);
+$val = system($command, $ret);
+
+if ($ret == 127) {
+	echo "\nPhpDocumentor tool not found\n\n";
+	echo "Please check that you have installed PhpDocumentor and you have phpdoc command in \$PATH\n";
+	echo "If you don't have PhpDocumentor install it from http://phpdoc.org/ and try again.\n\n";
+	exit(2);
+}
+
 
 
 
