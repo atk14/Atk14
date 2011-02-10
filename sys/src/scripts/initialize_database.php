66e9b6210d336eb51ba78e224668491d7c23e62d
yarri <jaromir.tomek@gmail.com>
2011-02-10 09:56:19 +0000

    a new script for database initialization

---
+++ sys/src/scripts/initialize_database.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?
+/**
+* Initializes database! :)
+* Creates all necessaries in there.
+*
+* At the moment only the table schema_magrations will be created.
+*
+* Usage:
+*  $ ATK14_ENV=DEVELOPMENT ./scripts/initialize_database.php
+*  $ ATK14_ENV=TEST ./scripts/initialize_database.php
+*/
+
+require_once(dirname(__FILE__)."/load.inc");
+
+$dbmole->doQuery("CREATE TABLE schema_magrations(version VARCHAR(255) PRIMARY KEY);");
+
