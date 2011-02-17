b1cdc37cc0d38ad637bff28deba511d74330ece7
mysutka <michal.vojan@gmail.com>
2011-02-17 16:36:30 +0000

    changed subpackage; more documentation

--- sys/src/atk14/helpers/block.unless.php
+++ sys/src/atk14/helpers/block.unless.php
@@ -1,9 +1,28 @@
 <?
 /**
-* Zobrazi obsah parametru content nebo vnitrek blokoveho tagu, pokud je parametr prazdny.
-*
-*  Jmeno objednavky: {unless content=$title}{t}nepojmenovana{/t}{/unless}
-*/
+ * Smarty block plugin.
+ *
+ * Displays content of parameter content or content of the block when the parameter is empty.
+ *
+ * <code>
+ *  Jmeno objednavky: {unless content=$title}{t}nepojmenovana{/t}{/unless}
+ * </code>
+ *
+ * @package Atk14
+ * @subpackage Helpers
+ */
+
+/**
+ * Smarty block function
+ *
+ * Reserved parameters:
+ * <ul>
+ *  <li>content- name of tested variable</li>
+ * </ul>
+ *
+ * @param array $params
+ * @param string $content
+ */
 function smarty_block_unless($params,$content,&$smarty,&$repeat){
 	if(isset($params["content"]) && strlen($params["content"])>0){
 		return $params["content"];
