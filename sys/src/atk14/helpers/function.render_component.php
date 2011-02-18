49e4375252c1ffa368cb02bba4a4f382bb789c55
yarri <jaromir.tomek@gmail.com>
2011-02-18 09:54:46 +0000

    improving render_component helper

--- sys/src/atk14/helpers/function.render_component.php
+++ sys/src/atk14/helpers/function.render_component.php
@@ -10,9 +10,16 @@
 function smarty_function_render_component($params,&$smarty){
 	if(!isset($params["controller"])){ $params["controller"] = $smarty->_tpl_vars["controller"]; }
 
+	if(!isset($params["action"])){ $params["action"] = "index"; }
+
 	$controller_params = $params;
 	unset($controller_params["controller"]);
 	unset($controller_params["action"]);
+
+	// pokud je parametr objekt, bude preveden volanim getId() na skalarni hodnotu
+	foreach($controller_params as $key => $v){
+		if(is_object($v)){ $controller_params[$key] = $v->getId(); }
+	}
 
 	$response = Atk14Dispatcher::ExecuteAction($params["controller"],$params["action"],array(
 		"render_layout" => false,
