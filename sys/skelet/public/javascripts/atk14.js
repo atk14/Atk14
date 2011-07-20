4e8d69e948b7d9b576d102f034b88d7800316aa7
yarri <jaromir.tomek@gmail.com>
2011-07-20 09:28:10 +0000

    copying last version of atk14.js into skelet

--- sys/skelet/public/javascripts/atk14.js
+++ sys/skelet/public/javascripts/atk14.js
@@ -1,146 +1,81 @@
-$(document).ready(function() {
-	ATK14.init();
-});
+var ATK14 = (function( $ ) {
 
-/*
- * ATK14 namespace
- *
- */
-var ATK14 = {
-	VERSION: '',
-	LANG: ''
-};
+	$( "a[data-remote]" ).live( "click", function() {
+		var $link = $( this ),
+			confirmMessage = $link.data( "confirm" );
 
-/*
- * First things first
- *
- */
-ATK14.init = function() {
-	// set a language for gettext
-	ATK14.LANG = $("meta[name='x-lang']").attr("content");
-	// convert obfuscated email addresses to normal mailto links
-	$('.atk14_no_spam').unobfuscate();
-	// attach live events on remote elements
-	$('a.remote_link').live('click', ATK14.Remote.handle_link);
-	$('form.remote_form').livequery('submit', ATK14.Remote.handle_form);
-	// global ajaxStart/ajaxStop events
-	$('body')
+		if ( !confirmMessage || confirm( confirmMessage ) ) {
+			ATK14.handleRemote( this );
+		}
+
+		return false;
+	});
+
+	$( "form[data-remote]" ).live( "submit", function() {
+		ATK14.handleRemote( this );
+		return false;
+	});
+
+	$( "body" )
 		.ajaxStart(function() {
-			$(this).addClass('loading');
+			$( this ).addClass( "loading" );
 		})
 		.ajaxStop(function() {
-			$(this).removeClass('loading');
+			$( this ).removeClass( "loading" );
 		});
-}
 
-/*
- * First approach to gettext implementation.
- * Language code is determined from the specified meta tag:
- *   <meta name="x-lang" content="cs" />
- */
-ATK14.gettext = function(msg) {
-	if (ATK14.LANG == 'cs') {
-		switch(msg) {
-			case 'Are you sure?':
-				return 'Jste si jistý(á)?';
-				break;
-		}
-	}
-	return msg;
-}
+	$.ajaxSetup({
+		converters: {
+			"text conscript": true
+		},
+		dataType: "conscript"
+	});
 
-/*
- * Module for handling remote elements.
- * We have 2 basic remote elements: remote links and remote forms.
- * Remote links default to GET method and remote forms default to POST method.
- * Remote link with class="post" will be handled with POST method though.
- *
- */
-ATK14.Remote = (function() {
 
-	// private scope
-	function randomizeUrlForAjax(url) {
-		if (url.indexOf('?') > 0) {
-			url = url + '&';
-		} else{
-			url = url + '?';
-		}
-		url = url + '_=' + (Math.random());
-		return url;
-	}
-
-	// an object with public methods is assigned to ATK14.Remote
 	return {
-		evaluateSourceForRemoteLink: function(source, $link) {
-			eval(source);
-		},
-		evaluateSourceForRemoteForm: function(source, $form) {
-			eval(source);
-		},
 
-		handle_link: function() {
-			var $a = $(this);
+		action: $( "meta[name='x-action']" ).attr( "content" ),
 
-			if ($a.hasClass("confirm") && !confirm(ATK14.gettext("Are you sure?"))) {
-				return false;
+		handleRemote: function( element ) {
+			var method, url, data, $link, $form,
+				$element = $( element ),
+				dataType = $element.data( "type" ) || $.ajaxSettings.dataType;
+
+			if ( $element.is("form") ) {
+				$form = $element; // remove later
+				method = $element.attr( "method" );
+				url = $element.attr( "action" );
+				data = $element.serializeArray();
+			} else {
+				$link = $element; // remove later
+				method = $element.data( "method" );
+				url = $element.attr( "href" );
+				data = null;
 			}
 
-			// is there any additional callback for remote link?
-			if (typeof before_remote_link != "undefined") {
-				var ret = before_remote_link($a);
-				if (!ret) {
-					return ret;
-				}
-			}
+			$.ajax({
+				url: url,
+				type: method || 'GET',
+				data: data,
+				dataType: dataType,
+				beforeSend: function(xhr, settings) {
+					//return fire(element, 'ajax:beforeSend', [ xhr, settings ]);
+				},
+				success: function( data, status, xhr ) {
+					$element.trigger( "ajax:success", [ data, status, xhr ] );
 
-			var params = {
-				$link: $a,
-				cache: false,
-				type: 'GET',
-				dataType: 'text',
-				data: null,
-				url: $a.attr('href'),
-				success: function(source) {
-					ATK14.Remote.evaluateSourceForRemoteLink(source,this.$link);
-					if (typeof after_remote_link != "undefined") {
-						after_remote_link(this.$link);
+					if ( dataType === "conscript" ) {
+						eval( data );
 					}
 				},
-				complete: function() {
-					$('.atk14_no_spam').unobfuscate();
-				}
-			}
-			// when dealing with .post link, we have to modify default params
-			if ($a.hasClass('post')) {
-				params = $.extend({}, params, { type: 'POST', data: '' });
-			}
-			/*
-			if ($a.hasClass('http_method_delete')) {
-				params = $.extend({}, params, { type: 'POST', data: '_method=delete' });
-			}*/
-
-			$.ajax(params);
-
-			return false;
-		},
-
-		handle_form: function() {
-			var $f = $(this);
-			$.ajax({
-				$form: $f,
-				cache: false,
-				type: 'POST',
-				url: randomizeUrlForAjax($f.attr('action')),
-				dataType: 'text',
-				data: $f.serialize(),
-				complete: function(){
-					$('.atk14_no_spam').unobfuscate();
+				complete: function( xhr, status ) {
+					$element.trigger( "ajax:complete", [ xhr, status ] );
 				},
-				success: function(source) {
-					ATK14.Remote.evaluateSourceForRemoteForm(source,this.$form);
+				error: function( xhr, status, error ) {
+					$element.trigger( "ajax:error", [ xhr, status, error ] );
 				}
 			});
-			return false;
 		}
 	};
-})();
+
+})( jQuery );
