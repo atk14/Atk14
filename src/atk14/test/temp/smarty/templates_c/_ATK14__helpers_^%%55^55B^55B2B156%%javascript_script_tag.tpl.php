70c089b20d52c1f23ab14a6e2383b25e98e0ce7e
Jaromir Tomek <jaromir.tomek@gmail.com>
2011-10-19 20:20:05 +0200

    fixing...

--- src/atk14/test/temp/smarty/templates_c/_ATK14__helpers_^%%55^55B^55B2B156%%javascript_script_tag.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2011-10-19 19:47:56
-         compiled from javascript_script_tag.tpl */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('function', 'javascript_script_tag', 'javascript_script_tag.tpl', 1, false),)), $this); ?>
-<?php echo smarty_function_javascript_script_tag(array('file' => "site.js"), $this);?>
-
-<?php echo smarty_function_javascript_script_tag(array('file' => "site.js",'media' => 'screen'), $this);?>
-
-<?php echo smarty_function_javascript_script_tag(array('file' => "nonexisting.js"), $this);?>
