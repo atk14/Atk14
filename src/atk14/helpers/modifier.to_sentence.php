a108474aba4753074ebb5a9918396b7d5ec8ee58
Jaromir Tomek <jaromir.tomek@gmail.com>
2011-11-16 09:12:44 +0100

    a new helper

---
+++ src/atk14/helpers/modifier.to_sentence.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Converts an array to a sentence.
+ *
+ * {$singers|@to_sentence} -> George Michael, Boy George and Jimmy Somerville
+ *
+ * You should not to forget the @ symbol in front of the helper`s name.
+ */
+function smarty_modifier_to_sentence($ar,$connector = 'and'){
+	$out = '';
+	$index = 0;
+	$sizeof = sizeof($ar);
+	foreach($ar as $item){
+		if($index==$sizeof-1){
+			$out .= " $connector $item";
+		}elseif($index>0){
+			$out .= ", $item";
+		}else{
+			$out .= "$item";
+		}
+		$index++;
+	}
+	return $out;
+}
