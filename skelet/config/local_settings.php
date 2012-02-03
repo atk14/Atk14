87911f4cbd5f2911485ef1e91229be462ffb8156
Jaromir Tomek <jaromir.tomek@gmail.com>
2012-02-03 16:06:33 +0100

    some translations in skelet

--- skelet/config/local_settings.php
+++ skelet/config/local_settings.php
@@ -1,12 +1,18 @@
 <?php
 /**
-* Either some parts of ATK14 system (i.e. mailing subsystem) or some third party libs
-* could be configured by constants or variables.
-* 
-* This file is the right place to do such configuration.
-*
-* You can inspect all ATK14 system`s constants in atk14/src/default_settings.inc
-*/
+ * Either some parts of ATK14 system (i.e. mailing subsystem) or some third party libs
+ * could be configured by constants or variables.
+ * 
+ * This file is the right place to do such configuration.
+ *
+ * You can inspect all ATK14 application`s constants in atk14/default_settings.php
+ * 
+ * All the application constants should be inspected by calling:
+ *	$ ./scripts/dump_settings
+ * 
+ * A certain constant should be inspected this way:
+ *	$ ./scripts/dump_settings DEFAULT_EMAIL
+ */
 
 define("SECRET_TOKEN","put_some_random_string_here");
 
