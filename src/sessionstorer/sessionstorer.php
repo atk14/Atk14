6d259eda99a888d55cd03a7054d42cdd974e88bd
Jaromir Tomek <jaromir.tomek@gmail.com>
2013-12-11 10:07:54 +0100

    [SessionStorer] Improved structures of session tables

--- src/sessionstorer/sessionstorer.php
+++ src/sessionstorer/sessionstorer.php
@@ -7,7 +7,7 @@ if(!defined("SESSION_STORER_SESSION_MAX_LIFETIME")){
 }
 
 if(!defined("SESSION_STORER_DEFAULT_SESSION_NAME")){
-	define("SESSION_STORER_DEFAULT_SESSION_NAME","ses");
+	define("SESSION_STORER_DEFAULT_SESSION_NAME","session");
 }
 
 if(!defined("SESSION_STORER_DEFAULT_SECTION")){
@@ -59,38 +59,7 @@ if(!defined("SESSION_STORER_SHARE_COOKIES_ON_SUBDOMAINS")){
  *			//...
  *		}
  *
- * There are two tables needed in a database:
- * <code>
- *			CREATE SEQUENCE seq_sessions;
- *			CREATE TABLE sessions(
- *				id INT NOT NULL PRIMARY KEY DEFAULT NEXTVAL('seq_sessions'),
- *				security VARCHAR(32) NOT NULL CHECK (security ~ '^[a-zA-Z0-9]{32}$'),
- *				session_name VARCHAR(64) NOT NULL CHECK(LENGTH(session_name)>0),
- *				--
- *				remote_addr VARCHAR(255) DEFAULT '' NOT NULL,
- *				--
- *				created TIMESTAMP DEFAULT NOW() NOT NULL,
- *				last_access TIMESTAMP DEFAULT NOW() NOT NULL
- *			);
- *			CREATE INDEX in_sessions_lastaccess ON sessions (last_access);
- *
- *			CREATE SEQUENCE seq_session_values;
- *			CREATE TABLE session_values(
- *				id INT NOT NULL PRIMARY KEY DEFAULT NEXTVAL('seq_session_values'),
- *				session_id INT NOT NULL,
- *				section VARCHAR(64) NOT NULL CHECK(LENGTH(section)>0),
- *				--
- *				key VARCHAR(128) NOT NULL CHECK(LENGTH(key)>0),
- *				value TEXT DEFAULT '' NOT NULL,
- *				expiration TIMESTAMP DEFAULT NULL,
- *				--
- *				CONSTRAINT unq_sessionvalues UNIQUE(session_id,session_name,key),
- *				CONSTRAINT fk_sessionvalues_sessions FOREIGN KEY (session_id) REFERENCES sessions(id) ON DELETE CASCADE
- *			);
- *			CREATE INDEX in_sessionvalues_sessionid ON session_values(session_id);
- *			CREATE INDEX in_sessionvalues_expiration ON session_values(expiration);
- * </code>
- *
+ * There are two tables needed in a database. See README for structure specifications.
  */
 class SessionStorer{
 
