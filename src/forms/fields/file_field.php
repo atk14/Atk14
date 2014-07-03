f8442a2d7b293a42869dc3db26b88eaec7210891
Jaromir Tomek <jaromir.tomek@gmail.com>
2014-07-03 15:04:56 +0200

    [Forms] Added allowed_mime_types option to FileField

--- src/forms/fields/file_field.php
+++ src/forms/fields/file_field.php
@@ -11,11 +11,15 @@ class FileField extends Field{
 	function __construct($options = array()){
 		$options += array(
 			"max_file_size" => null, // 1024, "1.5MB",
+			"allowed_mime_types" => array(), // array("application/pdf","image/svg+xml")
 			"widget" => new FileInput(),
 		);
 
 		$this->max_file_size = $this->_fileSize2Int($options["max_file_size"]);
 		unset($options["max_file_size"]);
+
+		$this->allowed_mime_types = $options["allowed_mime_types"];
+		unset($options["allowed_mime_types"]);
 
 		if($this->max_file_size){
 			$options["widget"]->attrs["data-max-file-size"] = $this->max_file_size;
@@ -25,14 +29,22 @@ class FileField extends Field{
 
 		$this->update_messages(array(
 			"file_too_big" => _('Ensure this file has at most %max_file_size% bytes (it has %file_size%)'),
+			"disallowed_mime_type" => _('Disallowed file type (%mime_type%)'),
 		));
 	}
+
 	function clean($value){
 		list($err,$value) = parent::clean($value);
 		if($err || !$value){ return array($err,$value); }
 		if($this->max_file_size && $value->getFileSize()>$this->max_file_size){
 			return array(	
 				strtr($this->messages["file_too_big"],array("%max_file_size%" => $this->max_file_size, "%file_size%" => $value->getFileSize())),
+				null
+			);
+		}
+		if($this->allowed_mime_types && !in_array($value->getMimeType(),$this->allowed_mime_types)){
+			return array(
+				strtr($this->messages["disallowed_mime_type"],array("%mime_type%" => h($value->getMimeType()))),
 				null
 			);
 		}
