cd11f14079068791cd472dd3f64d14ce0f4854b0
Jaromir Tomek <jaromir.tomek@gmail.com>
2014-07-28 11:21:13 +0200

    Collapsing/expanding variables displayed by helper dump

--- src/atk14/helpers/function.dump.php
+++ src/atk14/helpers/function.dump.php
@@ -29,14 +29,14 @@ function smarty_function_dump($params,$template){
 		sort($keys);
 		foreach($keys as $key){
 			$out[] = '<li>';
-			$out[] = '<pre><code>'.h($key).': '.h(Dumper::Dump($smarty->getTemplateVars($key))).'</code></pre>';
+			$out[] = '<pre><code>'.h('$'.$key).': '.Dumper::Dump($smarty->getTemplateVars($key),0,array('collapse_structure' => true)).'</code></pre>';
 			$out[] = '</li>';
 		}
 		$out[] = "</ul>";
 		return join("\n",$out);
 	}
 	$out = isset($params["var"]) ? Dumper::Dump($params["var"]) : "NULL";
-	return "<pre><code>".h($out)."</code></pre>";
+	return "<pre><code>$out</code></pre>";
 }
 
 /**
@@ -49,13 +49,13 @@ class Dumper{
 	 * echo Dumper::Dump(true); // [true]
 	 * echo Dumper::Dump(null); // NULL
 	 */
-	static function Dump($var,$offset = 0){
+	static function Dump($var,$offset = 0,$options = array()){
 		$dumper = new Dumper();
 
-		return $dumper->_dump($var,$offset);
+		return $dumper->_dump($var,$offset,$options);
 	}
 
-	function _dump($var,$offset){
+	function _dump($var,$offset,$options = array()){
 		if(!isset($var)){
 			return $this->_Pad("NULL",$offset);
 		}
@@ -63,15 +63,19 @@ class Dumper{
 			return $this->_Pad($var ? "[true]" : "[false]",$offset);
 		}
 		if(is_object($var)){
-			return $this->_Pad($this->_DumpObject($var,$offset),$offset);
+			return $this->_Pad($this->_DumpObject($var,$offset,$options),$offset);
 		}
 		if(is_array($var)){
-			return $this->_Pad($this->_DumpArray($var,$offset),$offset);
+			return $this->_Pad($this->_DumpArray($var,$offset,$options),$offset);
 		}
-		return $this->_Pad(print_r($var,true),$offset);	
+		return h($this->_Pad(print_r($var,true),$offset));	
 	}
 
-	function _DumpObject($obj,$offset){
+	function _DumpObject($obj,$offset,$options = array()){
+		$options += array(
+			"collapse_structure" => false,
+		);
+
 		if($offset>0){
 			// in case of a deeper nesting a structure of a object is not being displayed
 			return $this->_Pad(get_class($obj)." Object",$offset);
@@ -79,7 +83,8 @@ class Dumper{
 
 		if(method_exists($obj,$_method = "toArray") || method_exists($obj,$_method = "to_array")){
 			//return Dumper::_Pad(get_class($obj,$obj)." Object\n".preg_replace('/^Array\n/s','',print_r($obj->toArray(),true)),$offset);
-			return get_class($obj)." ".$this->_DumpArray($obj->toArray(),0,array("label" => "Object"));
+			$options["label"] = "Object";
+			return get_class($obj)." ".$this->_DumpArray($obj->toArray(),0,$options);
 		}
 
 		if(isset($obj->_beeing_dumped_by_dumper)){ return $this->_Pad("[recursion]",$offset); }
@@ -95,28 +100,56 @@ class Dumper{
 		}
 
 		$out[] = get_class($obj).' Object(';
+
 		foreach($attrs as $attr){
 			$out[] = "  [$attr] => ".trim($this->Dump($obj->$attr,1));
 			//$out[] = "  [$attr] => ...";
 		}
+
 		$out[] = ")";
 
+		if($options["collapse_structure"]){
+			$this->_makeCollapsible($out);
+		}
 
 		unset($obj->_beeing_dumped_by_dumper);
 
 		return $this->_Pad($out,$offset);
 	}
 
+	function _makeCollapsible(&$out){
+
+		// "Form Object("
+		// "Array("
+		preg_match('/^(.+)\($/',$out[0],$matchces);
+		$label = $matchces[1];
+
+		$id = "dump_".uniqid();
+		$id_to_be_hidden = $id."_h";
+	
+		// TODO: uf, to be rewritten somehow
+		$out[0] = '<span id="'.$id_to_be_hidden.'"><a href="#" onclick="JavaScript: document.getElementById(\''.$id.'\').style.display=\'inline\'; document.getElementById(\''.$id_to_be_hidden.'\').style.display=\'none\'; return false;" title="expand">'.$label.'(+)</a></span><span style="display:none;" id="'.$id.'"><a href="#" onclick="JavaScript: document.getElementById(\''.$id_to_be_hidden.'\').style.display=\'inline\'; document.getElementById(\''.$id.'\').style.display=\'none\'; return false;" title="collapse">'.$label.'(</a>';
+
+		$out[sizeof($out)-1] .= "</span>";
+	}
+
 	function _DumpArray($ar,$offset,$options = array()){
 		$options = array_merge(array(
 			"label" => "Array",
+			"collapse_structure" => false,
 		),$options);
+
 		$out = array();
 		$out[] = $options["label"].'(';
 		foreach($ar as $k => $v){
 			$out[] = " [$k] => ".trim($this->_Pad($this->Dump($v),1));
 		}
 		$out[] = ')';
+
+		if($options["collapse_structure"]){
+			$this->_makeCollapsible($out);
+		}
+
 		return $this->_Pad($out,$offset);
 	}
 
