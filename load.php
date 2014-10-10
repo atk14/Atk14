bfd2d6fdd7ed632b7e0e8694646b9d1cde2b11a4
Jaromir Tomek <jaromir.tomek@gmail.com>
2014-10-10 14:13:12 +0200

    Fix in floats localization

--- load.php
+++ load.php
@@ -145,6 +145,7 @@ function atk14_initialize_locale(&$lang){
 	setlocale(LC_ALL,$l);
 	setlocale(LC_CTYPE,$l);
 	setlocale(LC_COLLATE,$l);
+	setlocale(LC_NUMERIC,"C"); // we need to display float like 123.456
 	bindtextdomain("messages",dirname(__FILE__)."/../locale/");
 	bind_textdomain_codeset("messages", DEFAULT_CHARSET);
 	textdomain("messages");
