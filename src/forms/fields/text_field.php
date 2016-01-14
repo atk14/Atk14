8bd144f24fedbf9cf10a9b00db57772bdfcc6c2c
Jaromir Tomek <jaromir.tomek@gmail.com>
2016-01-14 10:15:20 +0100

    [Forms] TextField added

---
+++ src/forms/fields/text_field.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Field for long strings.
+ *
+ * TextField renders <textarea> HTML tag
+ * and preserves white spaces by default.
+ *
+ * @package Atk14
+ * @subpackage Forms
+ */
+class TextField extends CharField{
+
+	function __construct($options = array())
+	{
+		$options = forms_array_merge(array(
+			"widget" => new TextArea(),
+			"trim_value" => false,
+		),$options);
+		parent::__construct($options);
+	}
+
+	function clean($value){
+		if($this->required && trim($value)==""){
+			// when there are white characters only, the value is considered as empty
+			return array($this->messages["required"],null);
+		}
+		return parent::clean($value);
+	}
+}
