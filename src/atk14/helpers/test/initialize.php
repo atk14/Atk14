652da4aa538cd4ef143a4391dacf75286f3de316
Jaromir Tomek <jaromir.tomek@gmail.com>
2016-01-15 16:05:56 +0100

    [Atk14] Fixed Test Case initialization in order to run on Travis

--- src/atk14/helpers/test/initialize.php
+++ src/atk14/helpers/test/initialize.php
@@ -1,5 +1,5 @@
 <?php
 define("TEST",true);
-require("../../../../load.php");
-require("../function.to_json.php");
-require("../modifier.camelize.php");
+require(__DIR__."/../../../string/load.php");
+require(__DIR__."/../function.to_json.php");
+require(__DIR__."/../modifier.camelize.php");
