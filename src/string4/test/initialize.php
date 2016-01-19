e5425dd9d6676edc9f77cbe2aaf6bdad7daf3c01
Jaromir Tomek <jaromir.tomek@gmail.com>
2016-01-19 10:43:14 +0100

    [String4] Set encoding properly in tests

--- src/string4/test/initialize.php
+++ src/string4/test/initialize.php
@@ -1,5 +1,13 @@
 <?php
 define("DEFAULT_CHARSET","UTF-8");
 define("TRANSLATE_USE_ICONV",false);
+
+if(PHP_VERSION_ID < 50600){
+	function_exists('iconv_set_encoding') && iconv_set_encoding('internal_encoding',DEFAULT_CHARSET);
+	function_exists("mb_internal_encoding") && mb_internal_encoding(DEFAULT_CHARSET);
+} else {
+	ini_set('default_charset', DEFAULT_CHARSET);
+}
+
 require("../load.php");
 require("../../translate/translate.php");
