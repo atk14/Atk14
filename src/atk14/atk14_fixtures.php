bb59a43251206b8bd40c19e62a22392f368df7a9
Jaromir Tomek <jaromir.tomek@gmail.com>
2017-05-16 08:58:55 +0200

    [Fixtures] Working on fixtures feature

--- src/atk14/atk14_fixtures.php
+++ src/atk14/atk14_fixtures.php
@@ -12,25 +12,35 @@ class Atk14Fixtures {
 	 *
 	 * @return Atk14FixtureList
 	 */
-	static function Load($name){
+	static function Load($name,$options = array()){
 		global $ATK14_GLOBAL;
+
+		$options += array(
+			"model_class" => null, // ""
+		);
 
 		$filename = realpath(sprintf("%s/../test/fixtures/%s.yml", $ATK14_GLOBAL->getApplicationPath(), $name));
 		if (!file_exists($filename)) {
 			throw new Exception("Fixture $name not found");
 		}
 
-		$class_name = new String4($name);
-		$model_class_name = $class_name->singularize()->camelize()->toString(); // "gallery_items" -> "GalleryItem"
+		$model_class = $options["model_class"];
+		if(is_null($model_class)){
+			$model_class = String4::ToObject($name)->singularize()->camelize()->toString(); // "gallery_items" -> "GalleryItem"
+			if(!class_exists($model_class) || !method_exists($model_class,'CreateNewRecord')){
+				$model_class = "";
+			}
+		}
 
 		$yml = miniYAML::Load(Files::GetFileContent($filename));
 
-		$list = new Atk14FixturesList($model_class_name);
+		$list = new Atk14FixturesList($model_class);
 		foreach($yml as $k => $values) {
-			$o = $model_class_name::CreateNewRecord($values);
+			$o = $model_class::CreateNewRecord($values);
 			//$list[$k] = $o->getId();
 			$list[$k] = $o;
 		}
+
 		return $list;
 	}
 }
