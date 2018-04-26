a7e9021881fbc38fa05cd68143f8de1ef037de9e
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-04-26 15:52:12 +0200

    [Atk14] Block helper a_destroy accepts a new parameter _xhr (default true)

--- src/atk14/helpers/block.a_destroy.php
+++ src/atk14/helpers/block.a_destroy.php
@@ -7,22 +7,31 @@
  * @package Atk14\Helpers
  */
 
-if(!function_exists("smarty_block_a_remote")){
-	require_once(dirname(__FILE__)."/block.a_remote.php");
-}
 
 /**
  * Smarty plugin that generates link for destroy action.
  *
  * The generated link will lead to destroy action. Thanks to 'post' class the link will be handled with javascript and request sent as remote with POST method.
  *
+ * Usage:
+ * ```
+ * {a_destroy id=$book}Delete the book entry{/a_destroy}
+ *
+ * {* When the default action is not enough... *}
+ * {a_destroy action="books/destroy" id=$book}Delete the book entry{/a_destroy}
+ *
+ * {* A custom confirmation message *}
+ * {a_destroy id=$book _confirm="Are you sure to delete this book?"}Delete the book entry{/a_destroy}
+ *
+ * {* In special cases you might not want to use XHR requests for deletions *}
+ * {a_destroy id=$book _xhr=false}Delete the book entry{/a_destroy}
+ * ```
+ *
  * @param string $content Content of the {a_destroy}{/a_destroy} block.
  * @param array $params params
  * @param Smarty_Internal_Template $template
  * @param boolean &$repeat  repeat flag
  * @uses smarty_block_a_remote
- *
- *
  */
 function smarty_block_a_destroy($params, $content, $template, &$repeat){
 	if($repeat){ return; }
@@ -32,7 +41,13 @@ function smarty_block_a_destroy($params, $content, $template, &$repeat){
 		"action" => "destroy",
 		"_confirm" => _("Are you sure?"),
 		"_method" => "post",
+		"_xhr" => true,
 	),$params);
+
+	$xhr = $params["_xhr"];
+	unset($params["_xhr"]);
+
+	$xhr = is_string($xhr) ? String4::ToObject($xhr)->toBoolean() : $xhr;
 
 	// building attribute data-destroying_object
 	$object_id = is_object($params["id"]) ? $params["id"]->getId() : $params["id"];
@@ -47,5 +62,11 @@ function smarty_block_a_destroy($params, $content, $template, &$repeat){
 	);
 	$params["_data___destroying_object"] = json_encode($destroying_object);
 
-	return smarty_block_a_remote($params,$content,$template,$repeat);
+	if($xhr){
+		Atk14Require::Helper("block.a_remote");
+		return smarty_block_a_remote($params,$content,$template,$repeat);
+	}
+
+	Atk14Require::Helper("block.a");
+	return smarty_block_a($params,$content,$template,$repeat);
 }
