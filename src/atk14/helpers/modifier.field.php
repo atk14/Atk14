e58bdfb2852400a026006d01083afddd3c1b7e9d
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-10-08 18:12:00 +0200

    [Atk14] Smarty modifier field can be called with option "label_to_placeholder"

--- src/atk14/helpers/modifier.field.php
+++ src/atk14/helpers/modifier.field.php
@@ -1,10 +1,24 @@
 <?php
 /**
-* Vytahne field (HTML reprezentaci) formularoveho pole.
-*
-* {$form|field:"firstname"}
-*/
-function smarty_modifier_field($form, $field){
-	$field = $form->get_field($field);
+ * Renders a field (as widget) from the given form
+ *
+ *	{$form|field:"firstname"}
+ *
+ * Label can be copied to placeholder (in case of Input* fields)
+ *
+ *	{$form|field:"search":"label_to_placeholder"}
+ */
+function smarty_modifier_field($form, $field_name, $options = ""){
+	$options = Atk14Utils::StringToOptions($options);
+	$options += array(
+		"label_to_placeholder" => false,
+	);
+
+	$field = $form->get_field($field_name);
+
+	if($options["label_to_placeholder"] && is_subclass_of($field->widget,"Input") && !array_key_exists("placeholder",$field->widget->attrs)){
+		$field->widget->attrs["placeholder"] = $field->label;
+	}
+
 	return $field->as_widget();
 }
--- src/atk14/helpers/modifier.form_field.php
+++ src/atk14/helpers/modifier.form_field.php
@@ -1,9 +1,10 @@
 <?php
-require_once(dirname(__FILE__)."/modifier.field.php");
-
 /**
-* Vytahne field (HTML reprezentaci) formularoveho pole.
-*/
-function smarty_modifier_form_field($form, $field){
-	return smarty_modifier_field($form,$field);
+ * Renders a field (as widget) from the given form
+ *
+ * In fact this is just an alias for smarty modifier field.
+ */
+function smarty_modifier_form_field($form, $field_name, $options = ""){
+	Atk14Require::Helper("modifier.field");
+	return smarty_modifier_field($form,$field_name,$options);
 }
