591378802af8fa84c83ed6a894060e6e6a88bc70
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-03-09 14:15:37 +0100

    [Atk14] Test fixed

--- src/atk14/test/tc_migration.php
+++ src/atk14/test/tc_migration.php
@@ -2,6 +2,11 @@
 class TcMigration extends TcBase {
 
 	function test_SetDatabaseSchema(){
+		$schemas = $this->dbmole->selectRows("SELECT schema_name FROM information_schema.schemata");
+		if(!in_array("test",$schemas)){
+			$this->dbmole->doQuery("CREATE SCHEMA test");
+		}
+		
 		$this->assertEquals("public",TableRecord_DatabaseAccessor_Postgresql::GetDefaultDatabaseSchema());
 
 		Atk14Migration::SetDatabaseSchema("test");
