a41ae1aa463292a163e53eb9c7d4992ebb4770e7
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-02-08 11:23:26 +0100

    [StringBuffer] Added legacy file for loading the StringBuffer

---
+++ src/stringbuffer/stringbuffer.php
@@ -0,0 +1,5 @@
+<?php
+// Legacy file for loading StringBuffer.
+// TODO: To be removed in the future.
+trigger_error("Please, include file atk14/src/stringbuffer/load.php rather than atk14/src/stringbuffer/stringbuffer.php");
+require_once(__DIR__ . "/load.php");
