ad3f21ac4cb4078be100605b43794c1a53daf265
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-08-16 17:27:22 +0200

    [Atk14] Test fixed

--- src/atk14/test/tc_scripts.php
+++ src/atk14/test/tc_scripts.php
@@ -2,7 +2,16 @@
 class TcScripts extends TcBase {
 
 	function test__dbconsole_command(){
-		$cmd = `../../scripts/_dbconsole_command`;
-		$this->assertEquals("psql -U 'atk14_demo' 'atk14_demo' -h '127.0.0.1' -p '5432'\n",$cmd);
+		global $ATK14_GLOBAL;
+		$ATK14_GLOBAL = Atk14Global::GetInstance();
+
+		ob_start();
+		require("../../scripts/_dbconsole_command");
+		$cmd = ob_get_contents();
+		ob_end_clean();
+		
+		$cmd = preg_replace('/#.*?\n/','',$cmd); // "#!/usr/bin/env php" -> ""
+
+		$this->assertEquals("psql -U 'test' 'test' -h '127.0.0.1' -p '5432'\n",$cmd);
 	}
 }
