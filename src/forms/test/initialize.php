004899e179c597177ef91b858011672955a4b482
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-10-25 11:26:35 +0200

    [Forms] Small correction

--- src/forms/test/initialize.php
+++ src/forms/test/initialize.php
@@ -1,5 +1,6 @@
 <?php
 define("TEST",true);
+setlocale(LC_NUMERIC,"C"); // we need to display float like 123.456
 
 require_once("../../class_autoload/class_autoload.php");
 require_once("../../atk14/atk14_locale.php");
@@ -13,5 +14,3 @@ require_once("../../xmole/xmole.php");
 require_once("./custom_fields/url_field.php");
 require_once("../../translate/load.php");
 require_once("../../string4/load.php");
-
-setlocale(LC_NUMERIC,"C"); // we need to display float like 123.456
