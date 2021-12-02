a848465294ee2ad52b237f6a95d7167e18423d52
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-12-02 21:43:42 +0100

    [UrlFetcher] Testing

--- src/url_fetcher/test/tc_url_fetcher_via_command.php
+++ src/url_fetcher/test/tc_url_fetcher_via_command.php
@@ -7,5 +7,11 @@ class tc_url_fetcher_via_command extends tc_base {
 		$this->assertEquals(200,$f->getStatusCode());
 		$data = json_decode((string)$f->getContent(),true);
 		$this->assertEquals("GET",$data["method"]);
+
+		$cmd = __DIR__ . "/test_response.sh";
+		$f = new UrlFetcherViaCommand($cmd,"http://www.example.com/");
+		$this->assertEquals(201,$f->getStatusCode());
+		$this->assertEquals("TEST!",(string)$f->getContent());
+		$this->assertEquals("text/plain",$f->getContentType());
 	}
 }
---
+++ src/url_fetcher/test/test_response.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo HTTP/1.0 201 Created
+echo Content-Type: text/plain
+echo
+echo -n TEST!
