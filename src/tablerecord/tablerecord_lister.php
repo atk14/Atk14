c2fab421759784a9303e39779dc8cef4d7b900cb
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-12-13 13:45:04 +0100

    Revert "[TableRecord] Fix for PHP5.6"
    This reverts commit 30a745a2fb9ea005317c820c1c0b269462a1a317.

--- src/tablerecord/tablerecord_lister.php
+++ src/tablerecord/tablerecord_lister.php
@@ -652,7 +652,7 @@ class TableRecord_Lister implements ArrayAccess, Iterator, Countable {
 	/**
 	 * @ignore
 	 */
-	public function count(){
+	public function count():int {
 		$items = $this->getItems();
 		return count($items);
 	}
