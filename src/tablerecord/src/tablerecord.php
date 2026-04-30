1790ef626fa327947effe7164f7130f4fa2d1ae5
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-04-30 15:05:04 +0200

    * 46e33fdd - [TableRecord] Backward compatibility fix

--- src/tablerecord/src/tablerecord.php
+++ src/tablerecord/src/tablerecord.php
@@ -1351,9 +1351,10 @@ class TableRecord extends inobj {
 	 *
 	 * @return null
 	 */
-	function destroy():void {
+	function destroy(){
 		$this->_Hook_BeforeDestroy();
 		$this->dbmole->doQuery("DELETE FROM ".$this->dbmole->escapeTableName4Sql($this->getTableName())." WHERE ".$this->_escapeColumnName4Sql($this->getIdFieldName())."=:id",[":id" => $this->_Id]);
+		return null;
 	}
 
 	/**
