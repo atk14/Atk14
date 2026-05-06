bb4251040972f45cdb14c24e16c6911ac7fec5da
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-05-06 15:15:30 +0200

    [Atk14] sizeof() -> count()

--- src/atk14/helpers/modifier.count.php
+++ src/atk14/helpers/modifier.count.php
@@ -8,5 +8,5 @@
 */
 function smarty_modifier_count($ar){
 	if(is_null($ar)){ return 0; }
-	return sizeof($ar);
+	return count($ar);
 }
--- src/atk14/helpers/modifier.sizeof.php
+++ src/atk14/helpers/modifier.sizeof.php
@@ -1,5 +1,5 @@
 <?php
 function smarty_modifier_sizeof($ar){
 	if(is_null($ar)){ return 0; }
-	return sizeof($ar);
+	return count($ar);
 }
