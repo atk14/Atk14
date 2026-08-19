11ee1998422d91479cd01a1fe94b6518ce02c703
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-08-19 22:55:26 +0200

    [UrlFetcher] Tests fixed

--- src/url_fetcher/test/initialize.php
+++ src/url_fetcher/test/initialize.php
@@ -2,3 +2,5 @@
 define("TEMP",__DIR__ . "/tmp/");
 require("../src/url_fetcher.php");
 require("../src/url_fetcher_via_command.php");
+require("../../stringbuffer/load.php");
+require("../../files/load.php");
